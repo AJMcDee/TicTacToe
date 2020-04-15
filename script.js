fd4db1ed419206f937a15cf37ca657326655ce96
Anna McDougall <anna.mcdougall@gmail.com>
2020-04-15 10:29:22 +0200

    Basic player entry and square clicks working

--- script.js
+++ script.js
@@ -30,48 +30,85 @@ const gameBoard = (() => {
 
 // Game event handlers go here
 const game = (() => {
-
+    const playerList = ["",""];
     const startGameButton = document.getElementById("startgame")
-    startGameButton.addEventListener("click", function() {
-        gameBoard.gameBoard.style.cursor = "pointer"
-        runGame();
-    })
     const gamesquares = document.getElementsByClassName("gamesquare");
-    function runGame() {
+
+    startGameButton.addEventListener("click", setGameEnvironment)
+
+
+    function setGameEnvironment() {
+        let currentPlayer = "player1";
+        gameBoard.gameBoard.style.cursor = "pointer"
         for (let i = 0; i < gamesquares.length; i++) {
+            
+
+            const squareID = gamesquares[i].id;
             gamesquares[i].addEventListener("mouseenter", function () {
                 gamesquares[i].style.backgroundColor = "white";
             })
             gamesquares[i].addEventListener("mouseleave", function () {
                 gamesquares[i].style.backgroundColor = "aquamarine";
             })
+            gamesquares[i].addEventListener("click", function () {
+                if (currentPlayer === "player1") {
+                    gamesquares[i].style.color = game.playerList[0].color
+                    gameBoard.add(squareID,"X")
+                    currentPlayer = "player2"
+
+                } else {
+                    gamesquares[i].style.color = game.playerList[1].color
+                    gameBoard.add(squareID, "O");
+                    currentPlayer = "player1"
+                }  
+            })
         }
-
-
-
     }
 
 
 
-
     return {
-
+       playerList
     }
 })()
 
-
 // Player information goes here
-const Player = (name) => {
-    const add = (name) => {
-        
-    }
-    
+const Player = (name, color, playerID) => {
+
     return {
-        add,
+        name,
+        color,
+        playerID
     }
+
 }
 
 // Player settings go here
+var playerSetUp = (function playerSetUp() {
 
 
+    const submitButtons = document.getElementsByClassName("submitplayerbutton")
+
+        for (let i = 0; i < submitButtons.length; i++) {
+            const playerID = `player${i+1}`
+            const button = document.getElementById(`submit${playerID}`)
+            button.addEventListener("click", e => {
+                const playerName = document.getElementById(`${playerID}name`).value 
+                const playerColor = document.getElementById(`${playerID}shapecolor`).value
+                let newPlayer = Player(playerName, playerColor, playerID)
+                if (playerID === "player1") {
+                    game.playerList[0] = newPlayer
+                } else {
+                    game.playerList[1] = newPlayer
+                }
+            })
+
+        }
+
+
+    return {
+
+        }
+
+})()
 
